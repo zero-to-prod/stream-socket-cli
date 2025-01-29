bf29d77722712b9f4750f35e1adad23e0ab9ae85
david_smith <david_smith@sweetwater.com>
2025-01-29 15:39:56 -0500

    refactor/to-data-models Created `tests/Unit/GetOptionsCommandTest.php`.

---
+++ tests/Unit/GetOptionsCommandTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\Attributes\Test;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+use Tests\TestCase;
+use Zerotoprod\StreamSocketCli\GetOptions\GetOptionsArguments;
+use Zerotoprod\StreamSocketCli\GetOptions\GetOptionsCommand;
+use Zerotoprod\StreamSocketCli\SendTo\SendToArguments;
+use Zerotoprod\StreamSocketCli\SendTo\SendToCommand;
+
+class GetOptionsCommandTest extends TestCase
+{
+    #[Test] public function command(): void
+    {
+        $Application = new Application();
+        $Application->add(new GetOptionsCommand());
+        $Command = $Application->find(GetOptionsCommand::signature);
+        $CommandTester = new CommandTester($Command);
+
+        $CommandTester->execute([
+            GetOptionsArguments::url => GetOptionsArguments::url,
+        ]);
+
+        $CommandTester->assertCommandIsSuccessful();
+    }
+}
