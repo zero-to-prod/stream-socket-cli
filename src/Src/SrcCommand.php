a55b8bfcbe36241eacae543617e38beb90b28f23
david_smith <david_smith@sweetwater.com>
2025-01-29 15:39:55 -0500

    refactor/to-data-models Renamed `src/SrcCommand.php` to `src/Src/SrcCommand.php`.

--- src/Src/SrcCommand.php
+++ src/Src/SrcCommand.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Zerotoprod\StreamSocketCli;
+namespace Zerotoprod\StreamSocketCli\Src;
 
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
