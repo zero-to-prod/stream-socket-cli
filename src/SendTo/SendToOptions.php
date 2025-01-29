b170c9c8ea6c416a306b5dd8c456834e31b06863
david_smith <david_smith@sweetwater.com>
2025-01-29 15:39:55 -0500

    refactor/to-data-models Renamed `src/DataModels/SendToOptions.php` to `src/SendTo/SendToOptions.php`.

--- src/SendTo/SendToOptions.php
+++ src/SendTo/SendToOptions.php
@@ -1,18 +1,12 @@
 <?php
 
-namespace Zerotoprod\StreamSocketCli\DataModels;
+namespace Zerotoprod\StreamSocketCli\SendTo;
 
 use Zerotoprod\DataModel\DataModel;
 
 class SendToOptions
 {
     use DataModel;
-
-    public const url = 'url';
-    public string $url;
-
-    public const data = 'data';
-    public string $data;
 
     public const flags = 'flags';
     public int $flags = 0;
